4c7f6477863203ffc5cff0efae6066ff78d8ffe7
jonrovner <jonrovner@gmail.com>
2022-04-28 15:31:24 -0300

    added product section functionality

--- feapplication/src/redux/actions.js
+++ feapplication/src/redux/actions.js
@@ -5,6 +5,7 @@ export const GET_PRODUCT = "GET_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 
+
 export const get_config = (id) => async (dispatch) => {
   try {
     let res = await axios.get(`/configuration/${id}/`);
@@ -26,7 +27,7 @@ export const get_product = (id) => async (dispatch) => {
 
 export const update_product = (id, product) => async (dispatch) => {
   try {
-    let res = await axios.put(`/product/${id}`, product);
+    let res = await axios.put(`/product/${id}/`, product);
     console.log(res.data)
     return dispatch({ type: UPDATE_PRODUCT, payload: product });
   } catch (error) {
